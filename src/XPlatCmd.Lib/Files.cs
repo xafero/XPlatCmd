7a0541287365f348b9c1f75db695a8521da69f7f
xafero <xafero@users.noreply.github.com>
2026-05-10 18:26:04 +0200

    Fixing env vars

--- src/XPlatCmd.Lib/Files.cs
+++ src/XPlatCmd.Lib/Files.cs
@@ -8,6 +8,7 @@ namespace XPlatCmd.Lib
         public static string GetFullDir(string? dir)
         {
             var root = dir?.TrimOrNull() ?? Directory.GetCurrentDirectory();
+            root = Environment.ExpandEnvironmentVariables(root);
             root = Path.GetFullPath(root);
             if (!Directory.Exists(root))
                 throw new InvalidOperationException($"Directory not found => {root}");
